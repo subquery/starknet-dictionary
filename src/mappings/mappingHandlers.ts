4598c9e89eeb029fadb90b551d41c019dacb3aa6
JQQQ <jiqiang90@hotmail.com>
2025-01-19 15:02:20 +1300

    update mapping handler, fix missing data issue

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -5,7 +5,7 @@ import {flatten} from "lodash";
 
 export async function handleBlock(block: StarknetBlock): Promise<void> {
   const logs = block.logs.map((log, index) => handleLog(log,index));
-  const calls = await Promise.all(block.transactions.map(tx => handleTransaction(tx)));
+  const calls = block.transactions.map(tx => handleTransaction(tx));
   logger.info(`Block #${block.blockNumber}, Saving ${logs.length} logs and ${calls.length} calls`);
   await Promise.all([
     store.bulkCreate('Log', logs),
@@ -28,15 +28,15 @@ export function handleTransaction(transaction: StarknetTransaction): Call[] {
         case "DECLARE":
         case "DEPLOY":
         case "DEPLOY_ACCOUNT":
+        case "L1_HANDLER":
             calls.push(handleGenericCall(transaction));
             break;
-        case "L1_HANDLER":
         case "INVOKE":
             // we are expecting this not exist, as it parseCallData never been called.
             if(transaction.decodedCalls === undefined){
                 transaction.decodedCalls = transaction.parseCallData();
             }
-            calls.push(...handleInvokeL1Handler(transaction));
+            calls.push(...handleInvokeHandler(transaction));
             break;
 
         default:
@@ -60,8 +60,8 @@ function handleGenericCall(transaction: StarknetTransaction): Call {
     return call;
 }
 
-//Handle INVOKE and L1_HANDLER transaction
-function handleInvokeL1Handler(transaction: StarknetTransaction): Call[] {
+//Handle INVOKE transaction
+function handleInvokeHandler(transaction: StarknetTransaction): Call[] {
     const calls: Call[] = [];
     if (!transaction.decodedCalls) {
         return calls;
@@ -78,7 +78,9 @@ function handleInvokeL1Handler(transaction: StarknetTransaction): Call[] {
             throw new Error(`Transaction from is undefined, ${newCall.id},hash ${transaction.hash}`);
         }
         newCall.selector = call.selector;
+        newCall.to = call.to;
         newCall.type = transaction.type;
+        calls.push(newCall);
     }
     return calls;
 }
